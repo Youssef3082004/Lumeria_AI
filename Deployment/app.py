5a2a508f05760ed04f24fb8720689f5ab7fc7e1a
Merna Abass <mernaabass@outlook.com>
2026-03-27 21:19:13 +0200

    Add popular recommendations endpoint and refactor user API

--- Deployment/app.py
+++ Deployment/app.py
@@ -1,27 +1,36 @@
-import random 
+import random
 from fastapi import FastAPI
 from database import get_live_data
-from recommender import get_user_based_faiss
+from recommender import get_user_based_faiss, get_popular_list
 
 app = FastAPI(title="Lumeria AI API")
 
+@app.get("/")
+def root():
+    return {"status": "Lumeria Recommendation Engine is Online"}
+
+@app.get("/recommend/popular")
+def popular_api(location: str = None):
+    ratings_df, places_df = get_live_data()
+    return get_popular_list(ratings_df, places_df, location)
+
 @app.get("/recommend/user/{user_id}")
 def user_api(user_id: str):
-    # 1. سحب البيانات
+    # 1. سحب البيانات من سوبا بيز
     ratings_df, places_df = get_live_data()
     
-    # 2. محاولة جلب توصيات FAISS
+    # 2. محاولة جلب توصيات بناءً على ذوق اليوزر (FAISS)
     recommendations = get_user_based_faiss(user_id, ratings_df, places_df)
     
-    # 3. لو مفيش توصيات (يوزر جديد) -> 10 أماكن عشوائية تماماً
+    # 3. لو اليوزر جديد (النتائج فاضية) ->  أماكن عشوائية
     if not recommendations:
-        # تحويل الداتا لـ list من القواميس
-        all_places = places_df.to_dict(orient="records")
+        # تحويل جدول الأماكن لقائمة من القواميس
+        all_places_list = places_df.to_dict(orient="records")
+        num_to_select = min(10, len(all_places_list))
         
-        # اختيار 10 أماكن عشوائيةأو أقل
-        num_to_select = min(10, len(all_places))
-        random_places = random.sample(all_places, k=num_to_select)
+        # اختيار الأماكن العشوائية
+        random_recommendations = random.sample(all_places_list, k=num_to_select)
         
-        return random_places
-    
+        return random_recommendations
+
     return recommendations
