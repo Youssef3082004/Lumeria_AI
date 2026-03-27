747d50d07c3a31beb1c144a95db9ab3fb15c579d
Youssef Mustafa <awadallayossef@gmail.com>
2026-03-27 23:26:26 +0200

    Update Server

--- Deployment/app.py
+++ Deployment/app.py
@@ -1,9 +1,17 @@
-import random
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import random
 from database import get_live_data
-from recommender import get_user_based_faiss, get_popular_list
+from recommender import *
+import numpy as np 
+import faiss
 
 app = FastAPI(title="Lumeria AI API")
+app.add_middleware(CORSMiddleware,allow_origins=["*"],allow_methods=["*"],allow_headers=["*"])
+
+users_dataset , places = get_live_data()
+Places_embeddings_matrix = np.load("VectorDatabase/Places_Embeddings.npy")
+index = faiss.read_index("VectorDatabase/faiss_Places_Embeddings.index")
 
 @app.get("/")
 def root():
@@ -11,21 +19,17 @@ def root():
 
 @app.get("/recommend/popular")
 def popular_api(location: str = None):
-    ratings_df, places_df = get_live_data()
-    return get_popular_list(ratings_df, places_df, location)
+    return get_popular_list(users_dataset, places, location)
 
 @app.get("/recommend/user/{user_id}")
 def user_api(user_id: str):
-    # 1. سحب البيانات من سوبا بيز
-    ratings_df, places_df = get_live_data()
-    
     # 2. محاولة جلب توصيات بناءً على ذوق اليوزر (FAISS)
-    recommendations = get_user_based_faiss(user_id, ratings_df, places_df)
+    recommendations = get_user_based_faiss(user_id, users_dataset, places)
     
     # 3. لو اليوزر جديد (النتائج فاضية) ->  أماكن عشوائية
     if not recommendations:
         # تحويل جدول الأماكن لقائمة من القواميس
-        all_places_list = places_df.to_dict(orient="records")
+        all_places_list = places.to_dict(orient="records")
         num_to_select = min(10, len(all_places_list))
         
         # اختيار الأماكن العشوائية
@@ -34,3 +38,43 @@ def user_api(user_id: str):
         return random_recommendations
 
     return recommendations
+
+
+@app.get("/recommend/similarplaces/{place_id}")
+def similar_places(place_id:int, k:int = 10):
+    results = pd.DataFrame(get_similar_places(Places_embeddings_matrix=Places_embeddings_matrix,index=index,places=places,target_id=place_id, k=k + 1)).sort_values(by="distance", ascending=False)
+    return results.to_dict(orient="records")
+
+
+@app.get("/recommend/content/{user_id}")
+def Get_Similar_places_Content(user_id:str, k:int = 10):
+    top_Recommendation = 10
+    fav_places = users_dataset[(users_dataset["user_id"] == user_id) & (users_dataset["rating"] > 3)]["place_id"].values
+    k = len(fav_places) + top_Recommendation
+
+    
+    if len(fav_places) == 0:
+        return random.sample(places.to_dict(orient="records"),k=k) 
+        
+    fav_vectors = Places_embeddings_matrix[fav_places - 1]
+    
+    user_profile_vector = np.mean(fav_vectors, axis=0).reshape(1, -1).astype(np.float32)
+        
+    faiss.normalize_L2(user_profile_vector)
+    
+    distances, indices = index.search(user_profile_vector, k)
+    
+    distances = distances.flatten()
+    indices = indices.flatten()
+    
+    recommend = []
+    for dist, idx in zip(distances, indices):
+        place_id = places.iloc[idx]["ID"]
+        
+        if place_id not in fav_places:
+            row = places.iloc[idx].copy()
+            row["similarity_score"] = dist
+            recommend.append(row)
+            
+        if len(recommend) >= top_Recommendation:
+            return pd.DataFrame(recommend).to_dict(orient="records") 
